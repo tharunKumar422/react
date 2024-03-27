3b73b1f573bea5356360e1957c544509b057d6f8
R.K.THARUN  KUMAR <91961080+tharunKumar422@users.noreply.github.com>
2024-03-27 16:58:02 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const parent = React.createElement
+("div",{id: "parent"},[
+    React.createElement
+ ("div",{ id: "child" },
+ [
+    React.createElement
+ ("h1",{ id: "heading"},"im a h1 tag"),
+ React.createElement
+ ("h2",{ id: "heading"},"im a h2 tag")
+ ]
+ ),
+ React.createElement
+ ("div",{ id: "child" }, [
+    React.createElement
+ ("h1",{ id: "heading"},"im a h1 tag"),
+ React.createElement
+ ("h2",{ id: "heading"},"im a h2 tag")
+ ])
+]
+); 
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
